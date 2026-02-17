1c5738d2b30542a1b59662e1c61614423554b3eb
Rafael Mauricio Callisaya Goytia <rafael.mauricio.callisaya.goytia@alu.ceacfp.es>
2026-02-17 09:20:43 +0100

    Clase persona finalizada

---
+++ src/main/java/Person.java
@@ -0,0 +1,62 @@
+public class Person {
+
+    /// DECLARAR una VARIABLE
+    /// ATRIBUTOS
+    private int id;
+    private String name;
+    private String email;
+
+    /// Los frameworks  ORM (Hibernate/JPA) utilizan reflexion para crear instancias
+    /// de las clases cuando recuperan datos de la base de datos
+    public Person() {
+    }
+
+    /// -----  CONSTRUCTOR con todos los PARAMETROS ------
+    /// Este constructor se usa cuando creamos un objetos con todsos los datos conocidos
+    public Person(int id, String name, String email /* SON PARAMETROS */) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    ///  CONSTRUCTOR sin el id
+    public Person(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    /// METODOS
+    /// Permisos / Retorno / Nombre / (Parametros)
+    public String mostrarInfo(){
+        return "ID:" + id + "\n" +
+                "NOMBRE:" + name + "\n" +
+                "EMAIL:" + email;
+    }
+
+
+    ///  --- Getters y Setters ---
+
+    public int getId() {
+        return id;
+
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
+
--- src/main/java/Persona.java
+++
@@ -1,6 +0,0 @@
-public class Persona {
-    private String nombre;
-    private String surnames;
-    private int age;
-
-}
