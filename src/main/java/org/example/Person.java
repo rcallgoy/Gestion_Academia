2554d8358123fda48ea5e249884af83415c48586
Rafael Mauricio Callisaya Goytia <rafael.mauricio.callisaya.goytia@alu.ceacfp.es>
2026-02-17 09:58:01 +0100

    Clase alumno finalizada

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -40,8 +40,8 @@ public class Person {
 
     public int getId() {
         return id;
-
     }
+
     public void setId(int id) {
         this.id = id;
     }
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,50 @@
+package org.example;
+
+public class Student extends Person {
+
+
+    /// Atributos
+    private String curso;
+    private String notaMedia;
+
+    ///  ---Constructores---
+    ///  Constructor Vacio
+    public Student() {
+    }
+
+    /// Constructor completo con todos los parametros(contando con los de la clase padre)
+    public Student(int id, String name, String email, String curso, String notaMedia) {
+        super(id, name, email);
+        this.curso = curso;
+        this.notaMedia = notaMedia;
+    }
+
+    ///
+    public Student(String name, String email, String curso, String notaMedia) {
+        super(name, email);
+        this.curso = curso;
+        this.notaMedia = notaMedia;
+    }
+
+    /// ---Metodos---
+    @Override
+    public String mostrarInfo() {
+        return "Curso:" + curso + "\n" +
+                "NOMBRE:" + notaMedia;
+    }
+
+    /// Getter y setters
+    public String getCurso() {return curso;    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getNotaMedia() {
+        return notaMedia;
+    }
+
+    public void setNotaMedia(String notaMedia) {
+        this.notaMedia = notaMedia;
+    }
+}
